bfcd38aa9c434cbda60306b3d63531fb7cfdb4d7
uu29 <uu5474@gmail.com>
2021-12-19 14:32:12 +0900

    add: react-google-login update: 로그인 버튼 컴포넌트 분리

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,6 +3,25 @@ import { cx, css } from "@emotion/css";
 import { Logo } from "svgs";
 import Image from "next/image";
 
+const btn__google = css`
+  background: #fff;
+`;
+
+const btn__kakao = css`
+  background: #fee500;
+  color: #181600;
+`;
+
+const btn__naver = css`
+  background: #03c75a;
+  color: #fff;
+`;
+
+const btn__apple = css`
+  background: #000;
+  color: #fff;
+`;
+
 const login__btn = css`
   display: block;
   max-width: 30rem;
@@ -22,36 +41,60 @@ const login__logo = css`
   align-items: center;
 `;
 
+const GoogleBtn = () => {
+  return (
+    <button type="button" className={cx(login__btn, btn__google)}>
+      <span className={login__logo}>
+        <Image src="/images/logo__google.svg" alt="google logo" width={18} height={18} />
+      </span>
+      구글로 로그인
+    </button>
+  );
+};
+
+const KakaoBtn = () => {
+  return (
+    <button type="button" className={cx(login__btn, btn__kakao)}>
+      <span className={login__logo}>
+        <Image src="/images/logo__kakao.svg" alt="kakao logo" width={18} height={16.65} />
+      </span>
+      카카오로 로그인
+    </button>
+  );
+};
+
+const NaverBtn = () => {
+  return (
+    <button type="button" className={cx(login__btn, btn__naver)}>
+      <span className={login__logo}>
+        <Image src="/images/logo__naver.svg" alt="naver logo" width={15} height={14.88} />
+      </span>
+      네이버로 로그인
+    </button>
+  );
+};
+
+const AppleBtn = () => {
+  return (
+    <button type="button" className={cx(login__btn, btn__apple)}>
+      <span className={login__logo}>
+        <Image src="/images/logo__apple.svg" alt="apple logo" width={16} height={19.95} />
+      </span>
+      애플로 로그인
+    </button>
+  );
+};
+
 const Login = () => {
   return (
     <Section>
       <Logo />
       <Title>개미 매매일지</Title>
       <OauthWrap>
-        <GGLBtn type="button" className={login__btn}>
-          <span className={login__logo}>
-            <Image src="/images/logo__google.svg" alt="google logo" width={18} height={18} />
-          </span>
-          구글로 로그인
-        </GGLBtn>
-        <KKOBtn type="button" className={login__btn}>
-          <span className={login__logo}>
-            <Image src="/images/logo__kakao.svg" alt="kakao logo" width={18} height={16.65} />
-          </span>
-          카카오로 로그인
-        </KKOBtn>
-        <NVRBtn type="button" className={login__btn}>
-          <span className={login__logo}>
-            <Image src="/images/logo__naver.svg" alt="naver logo" width={15} height={14.88} />
-          </span>
-          네이버로 로그인
-        </NVRBtn>
-        <APLBtn type="button" className={login__btn}>
-          <span className={login__logo}>
-            <Image src="/images/logo__apple.svg" alt="apple logo" width={16} height={19.95} />
-          </span>
-          애플로 로그인
-        </APLBtn>
+        <GoogleBtn />
+        <KakaoBtn />
+        <NaverBtn />
+        <AppleBtn />
       </OauthWrap>
     </Section>
   );
@@ -60,22 +103,6 @@ const Login = () => {
 const OauthWrap = styled.div`
   margin-top: 12rem;
   width: 100%;
-`;
-
-const GGLBtn = styled.button`
-  background: #fff;
-`;
-const KKOBtn = styled.button`
-  background: #fee500;
-  color: #181600;
-`;
-const NVRBtn = styled.button`
-  background: #03c75a;
-  color: #fff;
-`;
-const APLBtn = styled.button`
-  background: #000;
-  color: #fff;
 `;
 
 const Section = styled.section`
