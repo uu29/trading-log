cd41b244e272516ef1bf9f7c068566f21a5506c0
uu29 <uu5474@gmail.com>
2021-12-22 23:30:37 +0900

    update: login 로직 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,25 +3,24 @@ import { cx, css } from "@emotion/css";
 import { Logo } from "svgs";
 import Image from "next/image";
 import { GoogleLogin } from "react-google-login";
+import { firebaseApp } from "firebase.config";
+import { getFirestore, collection, doc, setDoc, Timestamp, query, where, getDocs } from "firebase/firestore";
+const db = getFirestore(firebaseApp);
+const collection_name = "members";
 
 const btn__google = css`
   background: #fff;
 `;
 
-const btn__kakao = css`
-  background: #fee500;
-  color: #181600;
-`;
+// const btn__kakao = css`
+//   background: #fee500;
+//   color: #181600;
+// `;
 
-const btn__naver = css`
-  background: #03c75a;
-  color: #fff;
-`;
-
-const btn__apple = css`
-  background: #000;
-  color: #fff;
-`;
+// const btn__naver = css`
+//   background: #03c75a;
+//   color: #fff;
+// `;
 
 const login__btn = css`
   display: block;
@@ -42,10 +41,45 @@ const login__logo = css`
   align-items: center;
 `;
 
+type AsyncBool = Promise<boolean>;
+const AsyncBool = Promise;
+const checkDuplicate = async (email: string): AsyncBool => {
+  const newUserRef = collection(db, collection_name);
+  const q = query(newUserRef, where("email", "==", email));
+  const querySnapshot = await getDocs(q);
+  return Boolean(querySnapshot.size > 0);
+};
+
+const successCb = async (profileObj) => {
+  if (!profileObj) return;
+  const { email, name } = profileObj;
+  checkDuplicate(email).then((is_duplicate) => {
+    if (is_duplicate) {
+      alert("기존 회원입니다.");
+      return;
+    }
+  });
+
+  const reg_date = Timestamp.fromDate(new Date());
+  const newUserRefDoc = doc(collection(db, collection_name));
+
+  // id 자동 생성 후 회원가입하기
+  const data = { name, email, reg_date };
+
+  await setDoc(newUserRefDoc, data)
+    .then((res) => {
+      console.log("~~~~~~~~~~~~~");
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log("!!!!!!!!error!!!!!!!!");
+      console.log(err);
+    });
+};
+
 const GoogleBtn = () => {
-  const responseGoogle = (response) => {
-    console.log("[response]");
-    console.log(response);
+  const responseGoogle = async (response) => {
+    successCb(response.profileObj);
   };
 
   const failureGoogle = ({ error, details }) => {
@@ -73,38 +107,27 @@ const GoogleBtn = () => {
   );
 };
 
-const KakaoBtn = () => {
-  return (
-    <button type="button" className={cx(login__btn, btn__kakao)}>
-      <span className={login__logo}>
-        <Image src="/images/logo__kakao.svg" alt="kakao logo" width={18} height={16.65} />
-      </span>
-      카카오로 로그인
-    </button>
-  );
-};
+// const KakaoBtn = () => {
+//   return (
+//     <button type="button" className={cx(login__btn, btn__kakao)}>
+//       <span className={login__logo}>
+//         <Image src="/images/logo__kakao.svg" alt="kakao logo" width={18} height={16.65} />
+//       </span>
+//       카카오로 로그인
+//     </button>
+//   );
+// };
 
-const NaverBtn = () => {
-  return (
-    <button type="button" className={cx(login__btn, btn__naver)}>
-      <span className={login__logo}>
-        <Image src="/images/logo__naver.svg" alt="naver logo" width={15} height={14.88} />
-      </span>
-      네이버로 로그인
-    </button>
-  );
-};
-
-const AppleBtn = () => {
-  return (
-    <button type="button" className={cx(login__btn, btn__apple)}>
-      <span className={login__logo}>
-        <Image src="/images/logo__apple.svg" alt="apple logo" width={16} height={19.95} />
-      </span>
-      애플로 로그인
-    </button>
-  );
-};
+// const NaverBtn = () => {
+//   return (
+//     <button type="button" className={cx(login__btn, btn__naver)}>
+//       <span className={login__logo}>
+//         <Image src="/images/logo__naver.svg" alt="naver logo" width={15} height={14.88} />
+//       </span>
+//       네이버로 로그인
+//     </button>
+//   );
+// };
 
 const Login = () => {
   return (
@@ -113,9 +136,8 @@ const Login = () => {
       <Title>개미 매매일지</Title>
       <OauthWrap>
         <GoogleBtn />
-        <KakaoBtn />
-        <NaverBtn />
-        <AppleBtn />
+        {/* <KakaoBtn />
+        <NaverBtn /> */}
       </OauthWrap>
     </Section>
   );
