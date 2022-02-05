50df0df33c2aafa565ff77f28d9b903fe94fbfdc
uu29 <uu5474@gmail.com>
2022-02-05 23:40:30 +0900

    update: form 스타일링

--- components/calendar/CreateSchedule.tsx
+++ components/calendar/CreateSchedule.tsx
@@ -7,14 +7,19 @@ import { IUserCalendar } from "interface";
 
 const label_block = css`
   display: flex;
-  align-items: flex-start;
-  margin-bottom: 1.6rem;
+  align-items: center;
+  margin-bottom: 1rem;
   height: 3.6rem;
+`;
+
+const label_inline = css`
+  display: inline-flex;
+  align-items: center;
+  margin-bottom: 1rem;
 `;
 
 const label_icon = css`
   display: inline-block;
-  margin-top: 0.7rem;
   margin-right: 1.4rem;
   width: 2rem;
   height: 2rem;
@@ -53,7 +58,7 @@ const CreateSchedule = () => {
   return (
     <CreateScheduleWrap>
       <form method="post" onSubmit={handleSubmit}>
-        <TitleLabel htmlFor="title" className={label_block}>
+        <TitleLabel htmlFor="title">
           {!form.title && showPHolder && <TitleLabelText className={placeholder_color}>일정 제목 추가</TitleLabelText>}
           <TitleInput
             onFocus={onFocusTitle}
@@ -72,51 +77,71 @@ const CreateSchedule = () => {
           </DateValue>
           <input id="date" type="text" hidden value={form.date.seconds} />
         </label>
-        <TimeAlrtArea>
-          <TimeLabel htmlFor="time" className={label_block}>
-            <ClockIcon className={label_icon} />
-            <TimeLabelText className={placeholder_color}>시간 추가</TimeLabelText>
-            {/* <input id="time" type="time" value={form?.time?.seconds ?? undefined} /> */}
-          </TimeLabel>
+        <TimeLabel htmlFor="time" className={label_inline}>
+          <ClockIcon className={label_icon} />
+          <TimeLabelText className={placeholder_color}>시간 추가</TimeLabelText>
+          {/* <input id="time" type="time" value={form?.time?.seconds ?? undefined} /> */}
+        </TimeLabel>
+        <div className={label_inline}>
           <BellIcon />
-          <SwitchBox>
-            <label htmlFor="alert">
+          <label htmlFor="alert">
+            <SwitchBox>
               <input id="alert" name="alert" type="checkbox" checked={form.alert} onChange={handleChangeCheckbox} hidden />
-              <AlrtController checked={form.alert} />
-            </label>
-          </SwitchBox>
-          <AlrtText>알람 꺼짐</AlrtText>
-        </TimeAlrtArea>
-        <label htmlFor="content" className={label_block}>
-          설명 추가
-          <textarea id="content" value={form.content} />
-        </label>
+              <AlrtController isChecked={form.alert} />
+            </SwitchBox>
+          </label>
+          <AlrtText isChecked={form.alert}>{form.alert ? "알람 켜짐" : "알람 꺼짐"}</AlrtText>
+        </div>
+        <ContentArea>
+          <EditIcon>설명 추가</EditIcon>
+          <ContentText id="content" name="content" value={form.content} rows={4} cols={42} onChange={handleChange} />
+        </ContentArea>
       </form>
     </CreateScheduleWrap>
   );
 };
 
-const SwitchBox = styled.div`
+const ContentArea = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+
+const ContentText = styled.textarea`
+  padding: 0.6rem 0.8rem;
+  border: 0 none;
+  outline: 0 none;
+  resize: none;
+  font-size: 1.6rem;
+  line-height: 1.4;
+  background: #f6f7f9;
+`;
+
+const SwitchBox = styled.span`
+  display: block;
   position: relative;
-  width: 5rem;
+  width: 4.4rem;
   height: 2.4rem;
   border-radius: 1.2rem;
   border: 2px solid #8c91a5;
+  cursor: pointer;
 `;
 
-const AlrtController = styled.div<{ checked: boolean }>`
+const AlrtController = styled.div<{ isChecked: boolean }>`
   position: absolute;
+  top: 0;
+  left: 0;
   margin: 2px;
   width: 1.6rem;
   height: 1.6rem;
   border-radius: 8rem;
-  cursor: pointer;
-  ${({ checked }) => (checked ? "background: #B3B6C3" : "background: #2D96F6")};
+  ${({ isChecked }) => (isChecked ? "background: #2D96F6; transform: translateX(2rem);" : "background: #B3B6C3;")};
   transition: all 0.3s;
 `;
 
-const TimeAlrtArea = styled.div`
-  display: flex;
+const AlrtText = styled.span<{ isChecked: boolean }>`
+  margin-left: 1rem;
+  color: ${({ isChecked }) => (isChecked ? "#54555c" : "#c1c5d8")};
+  transition: all 0.3s;
 `;
 
 const TimeLabel = styled.label`
@@ -124,7 +149,6 @@ const TimeLabel = styled.label`
 `;
 
 const TimeLabelText = styled.span`
-  margin-top: 0.8rem;
   font-size: 1.7rem;
 `;
 
@@ -143,8 +167,15 @@ const DateValue = styled.button`
   }
 `;
 
-const AlrtText = styled.span`
-  color: #c1c5d8;
+const EditIcon = styled.span`
+  display: block;
+  width: 2.3rem;
+  height: 2.5rem;
+  margin-right: 1rem;
+  margin-left: 3px;
+  text-indent: -9999px;
+  background: url(/images/ico__edit.svg) no-repeat center;
+  background-size: contain;
 `;
 
 const CalIcon = styled.span`
@@ -157,7 +188,7 @@ const ClockIcon = styled.span`
 
 const BellIcon = styled.span`
   display: inline-block;
-  margin: 0.7rem 1rem 0 1.4rem;
+  margin: 0 1rem 0 2rem;
   width: 2rem;
   height: 2rem;
   text-indent: -9999px;
@@ -167,8 +198,10 @@ const BellIcon = styled.span`
 
 const TitleLabel = styled.label`
   position: relative;
+  display: block;
   margin-left: 3.4rem;
-  padding-bottom: 4px;
+  margin-bottom: 1.6rem;
+  padding-bottom: 6px;
   border-bottom: 2px solid #2d96f6;
 `;
 
@@ -176,11 +209,11 @@ const TitleLabelText = styled.span`
   position: absolute;
   left: 0;
   top: 0;
-  font-size: 2rem;
+  font-size: 1.8rem;
 `;
 
 const TitleInput = styled.input`
-  font-size: 2rem;
+  font-size: 1.8rem;
   outline: 0 none;
   border: 0 none;
 `;
@@ -191,10 +224,8 @@ const CreateScheduleWrap = styled.div`
   left: 10px;
   top: 10px;
   width: 100%;
-  height: 100%;
   padding: 2rem;
   max-width: 46rem;
-  max-height: 30rem;
   trasnform: traslate(-50%, -50%);
   background: #fff;
   box-shadow: 5px 5px 60px -10px rgba(66, 74, 106, 0.4);
